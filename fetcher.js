577b6c2faad61df181f81d98f68c659a6abd5847
affaf <affaf.rai@hotmail.com>
2020-11-17 02:52:19 +0000

    page downloader

--- fetcher.js
+++ fetcher.js
@@ -1,11 +1,6 @@
 const http = require('http');
 const fs = require('fs');
 
-function getFilesizeInBytes(filename) {
-  const stats = fs.statSync(filename);
-  const fileSizeInBytes = stats.size;
-  return fileSizeInBytes;
-}
 
 const file = fs.createWriteStream("./index.html");
 const request = http.get("http://www.example.edu/", function(response) {
